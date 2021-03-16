52009ea72e81ea7fe65e9942ca3ab416664f503a
Rafael Muttoni <hi@rafaelmuttoni.com>
2021-03-16 10:47:23 -0300

    not found page

--- src/views/errors/NotFoundView.js
+++ src/views/errors/NotFoundView.js
@@ -1,57 +1,55 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 import {
   Box,
   Container,
   Typography,
-  makeStyles
-} from '@material-ui/core';
-import Page from 'src/components/Page';
+  makeStyles,
+  Button,
+} from "@material-ui/core";
+import Page from "src/components/Page";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: theme.palette.background.dark,
-    height: '100%',
+    height: "100%",
     paddingBottom: theme.spacing(3),
-    paddingTop: theme.spacing(3)
+    paddingTop: theme.spacing(3),
   },
   image: {
     marginTop: 50,
-    display: 'inline-block',
-    maxWidth: '100%',
-    width: 560
-  }
+    display: "inline-block",
+    maxWidth: "100%",
+    width: 560,
+  },
 }));
 
 const NotFoundView = () => {
   const classes = useStyles();
 
   return (
-    <Page
-      className={classes.root}
-      title="404"
-    >
+    <Page className={classes.root} title="404">
       <Box
         display="flex"
         flexDirection="column"
         height="100%"
         justifyContent="center"
       >
-        <Container maxWidth="md">
+        <Container maxWidth="md" style={{ textAlign: "center" }}>
           <Typography
             align="center"
             color="textPrimary"
             variant="h1"
+            gutterBottom
           >
-            404: The page you are looking for isn’t here
+            404: Página não encontrada
           </Typography>
-          <Typography
-            align="center"
-            color="textPrimary"
-            variant="subtitle2"
-          >
-            You either tried some shady route or you came here by mistake.
-            Whichever it is, try using the navigation
-          </Typography>
+          <Link to="/">
+            <Button variant="contained" color="primary">
+              Clique aqui para voltar para a Página Inicial
+            </Button>
+          </Link>
+
           <Box textAlign="center">
             <img
               alt="Under development"
