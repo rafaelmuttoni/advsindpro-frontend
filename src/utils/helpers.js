e24eaeafb3618092724925daf53fea6240216d50
Rafael Muttoni <hi@rafaelmuttoni.com>
2021-04-16 14:46:12 -0300

    prebdays

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,27 +1,41 @@
-import moment from "moment";
+import moment from 'moment'
 
 export const calculateNextBirthdays = (condos) => {
   const birthdays = condos.map((condo) => {
-    const { id, name, initial_date } = condo;
+    const { id, name, initial_date } = condo
 
-    const initial = moment(initial_date).format("YYYY-MM-DD");
-    const numberOfYears = moment().diff(initial, "years");
+    const initial = moment(initial_date).format('YYYY-MM-DD')
+    const numberOfYears = moment().diff(initial, 'years')
 
     const bdays = [...Array(numberOfYears + 3)].map((x, i) => {
       return {
         id: `birthday C${id}-Y${i}`,
-        type: "birthdays",
+        type: 'birthdays',
         title: `Aniversário de Síndico`,
-        start: moment(initial_date).add(i, "y").format("YYYY-MM-DD"),
+        start: moment(initial_date).add(i, 'y').format('YYYY-MM-DD'),
         description: `Aniversário de ${i} anos no ${name}`,
         condo_id: id,
-      };
-    });
+      }
+    })
 
-    return bdays;
-  });
+    const preBdays = [...Array(numberOfYears + 3)].map((x, i) => {
+      return {
+        id: `prebirthday C${id}-Y${i}`,
+        type: 'birthdays',
+        title: `Pré Aniversário de Síndico`,
+        start: moment(initial_date)
+          .add(i, 'y')
+          .subtract(1, 'month')
+          .format('YYYY-MM-DD'),
+        description: `1 mês para aniversário de ${i} anos no ${name}`,
+        condo_id: id,
+      }
+    })
 
-  return birthdays.flat();
-};
+    return [...bdays, ...preBdays]
+  })
 
-export const debtCalculator = () => {};
+  return birthdays.flat()
+}
+
+export const debtCalculator = () => {}
