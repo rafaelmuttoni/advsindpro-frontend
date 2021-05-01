e73d1870ce1c532aebcb64980891bb1462812483
Rafael Muttoni <hi@rafaelmuttoni.com>
2021-05-01 17:38:40 -0300

    removed settings page p2

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import ProtectedRoute from "./ProtectedRoute";
-import OpenRoute from "./OpenRoute";
+import ProtectedRoute from './ProtectedRoute'
+import OpenRoute from './OpenRoute'
 
-import LoginView from "src/views/auth/LoginView";
+import LoginView from 'src/views/auth/LoginView'
 
-import HomeView from "src/views/home";
-import CalendarView from "src/views/calendar";
-import { CalendarProvider } from "src/views/calendar/CalendarContext";
-import ResidentsView from "src/views/residents";
-import ProvidersView from "src/views/providers";
-import DebtsView from "src/views/debts";
-import SettingsView from "src/views/settings/SettingsView";
+import HomeView from 'src/views/home'
+import CalendarView from 'src/views/calendar'
+import { CalendarProvider } from 'src/views/calendar/CalendarContext'
+import ResidentsView from 'src/views/residents'
+import ProvidersView from 'src/views/providers'
+import DebtsView from 'src/views/debts'
 
-import NotFoundView from "src/views/errors/NotFoundView";
+import NotFoundView from 'src/views/errors/NotFoundView'
 
 export default function Routes() {
   return (
@@ -41,14 +40,11 @@ export default function Routes() {
         <ProtectedRoute path="/debts">
           <DebtsView />
         </ProtectedRoute>
-        <ProtectedRoute path="/settings">
-          <SettingsView />
-        </ProtectedRoute>
 
         <Route path="*">
           <NotFoundView />
         </Route>
       </Switch>
     </Router>
-  );
+  )
 }
